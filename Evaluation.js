7f07900a331cc1f07b3d9fca797845c7bc29085a
Rouqaya1 <126081002+Rouqaya1@users.noreply.github.com>
2023-03-21 16:50:18 +0100

    Initial commit

---
+++ Evaluation.js
@@ -0,0 +1,27 @@
+// ==================SIBA Rouqaya EXERCICE 21/03/23=====================================
+// 1.
+let age = 25; 
+// 2.
+let nom = "Jean";
+// 3.
+let somme = 2 + 8;
+// 4.
+let chaine = "bon";
+chaine.toUpperCase();
+// 5.
+let tableau = [1,2,3,4,5];
+// avec de l'aide
+tableau.length;
+// 6.
+let objet = {
+    nom : "non",
+    age : 12
+// avec de l'aide
+};
+// 7. 
+let nombre = toFixed(30.6123456);
+// 8.
+let Dates = Dates.getMonth();
+// 9.
+let tableau2 = ["BOF", "Bof" , "Koussi", "Koussa", "moyen"]; 
+tableau.join(",");
