5290cccece66b0619f833d1d7e0ac4d16abf3ca9
Rouqaya1 <126081002+Rouqaya1@users.noreply.github.com>
2023-03-23 16:09:52 +0100

    Create Eval-tableau.js

---
+++ Eval-tableau.js
@@ -0,0 +1,24 @@
+// ================= SIBA ROUQAYA EVAL 23/03/23
+
+
+// 1.
+var tableau = ["Pierre", "Paul", "Jacques"];
+console.log(tableau);
+
+// 2.
+var tab = [1,2,3,4,5];
+for (var i = 0; i < tab.length; i++){
+    alert(tab[i]);
+}
+// 3.
+var array = [1,2,3,4,5];
+array.push(6);
+// 4.
+var tableaux = [1,2,3,4,5]
+tableaux.pop(4);
+// 5.
+var tables = [1,2,3,4,5]
+tables.unshift(0);
+// 6.
+var table = [1,2,3,4,5]
+table.shift(0);
